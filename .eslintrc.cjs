25423e827c8bb64d84a9af390d951957751ac1c3
Keisuke Watanuki <watanuki@ju-zen.com>
2023-12-10 00:50:30 +0900

    Run format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
-    },
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@stylistic/recommended-extends"
-      ],
-    "rules": {}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@stylistic/recommended-extends',
+  ],
+  rules: {},
 }
